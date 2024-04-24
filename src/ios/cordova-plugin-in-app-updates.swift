29fd09809a9724e7072fd11e124219239a269ee3
Alexander <iburundugi@gmail.com>
2024-04-24 18:01:11 +0300

    Update cordova-plugin-in-app-updates.swift
    add background thread

--- src/ios/cordova-plugin-in-app-updates.swift
+++ src/ios/cordova-plugin-in-app-updates.swift
@@ -2,20 +2,22 @@ import UIKit
 @objc(InAppUpdate) class InAppUpdate : CDVPlugin {
 
     @objc(getUpdateAvailability:) func getUpdateAvailability(command: CDVInvokedUrlCommand){
-        let status = newUpdates()
-        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: status)
-
-        commandDelegate.send(result, callbackId: command.callbackId)
+        self.commandDelegate.run(inBackground: {
+            let status = self.newUpdates()
+            let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: status)
+            
+            self.commandDelegate.send(result, callbackId: command.callbackId)
+        })
 
     }
-
+    
     func newUpdates() -> String{
         let infoDict = Bundle.main.infoDictionary! as NSDictionary
-
+        
         let appId = infoDict.object(forKey: "CFBundleIdentifier") as! String
         let url = NSURL(string: String(format: "http://itunes.apple.com/lookup?bundleId=%@", appId))
         let data = NSData(contentsOf: url! as URL)
-
+        
         do {
             let lookUp = try JSONSerialization.jsonObject(with: data! as Data, options: .mutableContainers) as? NSMutableDictionary
             if((lookUp?.object(forKey: "resultCount") as AnyObject).integerValue == 1){
@@ -31,11 +33,11 @@ import UIKit
                 return "UPDATE_NOT_AVAILABLE";
             }
         } catch {
-
+            return "UPDATE_NOT_AVAILABLE";
         }
-        return "UPDATE_NOT_AVAILABLE";
-    }
 
+    }
+    
     @objc(updateImmediate:) func updateImmediate(command: CDVInvokedUrlCommand) {
         guard let appStoreURI = URL(string: "itms-apps://apps.apple.com/app/id1560625254?mt=8") else {
             return
@@ -49,10 +51,10 @@ import UIKit
             if(UIApplication.shared.canOpenURL(appStoreURL)) {
                 UIApplication.shared.open(appStoreURL)
             }
-
+    
         }
     }
-
+    
 
 }
 
